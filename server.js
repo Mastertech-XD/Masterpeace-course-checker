887b14d07cbf5bd6f69790321f04f75446d02baf
Mastetech-XD <masterpeaceelite@gmail.com>
2025-04-01 01:10:52 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,34 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("server is running!");
+    res.send("<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KUCCPS Course Checker</title>
+    <link rel="stylesheet" href="static/styles.css">
+</head>
+<body>
+    <h1>KUCCPS Course & University Qualification Checker</h1>
+    <form id="gradesForm">
+        <label for="math">Mathematics:</label>
+        <input type="number" id="math" name="math" required><br>
+        <label for="eng">English:</label>
+        <input type="number" id="eng" name="eng" required><br>
+        <label for="kis">Kiswahili:</label>
+        <input type="number" id="kis" name="kis" required><br>
+        <label for="sci">Science:</label>
+        <input type="number" id="sci" name="sci" required><br>
+        <label for="hum">Humanities:</label>
+        <input type="number" id="hum" name="hum" required><br>
+        <button type="submit">Check Qualification</button>
+    </form>
+    <div id="results"></div>
+    
+    <script src="static/script.js"></script>
+</body>
+</html>");
 });
 
 app.listen(PORT, () => {
