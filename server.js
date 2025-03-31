924defb29a65a01ba0fcd301bfda1f01aeabff0e
Mastetech-XD <masterpeaceelite@gmail.com>
2025-04-01 01:05:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("yoh!");
+    res.send("server is running!");
 });
 
 app.listen(PORT, () => {
